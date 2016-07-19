e5498f3c41143aa6ac1302b10524aa79718870f4
shashankm <shashank.shashi2@gmail.com>
2016-07-19 14:14:56 +0530

    Finish showing feed in feeds tab

--- Feed.js
+++ Feed.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
-import { View, StyleSheet, Image, ListView, Text } from 'react-native'
+import { View, StyleSheet, Image, ListView, Text, ActivityIndicator } from 'react-native'
+
+var moment = require('moment')
 
 class Feed extends Component {
 	constructor(props) {
@@ -19,7 +21,7 @@ class Feed extends Component {
   	fetchData() {
   		require('./AuthService').getAuthInfo((err, authInfo) => {
   			var url = 'https://api.github.com/users/'
-  					+ authInfo.login + '/events'
+  					+ authInfo.login + '/received_events'
   			fetch(url, {
   				headers: authInfo.header
   			})		
@@ -34,16 +36,45 @@ class Feed extends Component {
   	}
 
   	renderFeed(feed) {
-  		debugger
+      let event = ''
+      switch(feed.type) {
+        case 'PushEvent':
+          event = ' pushed to '
+          break
+
+        case 'WatchEvent':
+          event = ' starred '
+          break
+
+        case 'CreateEvent':
+          event = ' created '
+          break
+
+        case 'ForkEvent':
+          event = ' forked '
+          break      
+      } 
   		return(
-  			<View><Text>{feed}</Text></View>
+  			<View style={styles.container}>
+          <Image source={{uri: feed.actor.avatar_url}} 
+                  style={styles.avatarPic} />
+            <View style={styles.userInfo}>      
+              <Text style={styles.userName}>{feed.actor.display_login}
+                <Text style={styles.event}>{event}</Text></Text>
+              <Text style={styles.repoName}>{feed.repo.name}</Text>
+              <Text style={styles.timeAgo}>{moment(feed.created_at).fromNow()}</Text>
+            </View>        
+        </View>
   		)
   	}
 
 	render() {
 		if (!this.state.loaded) {
 			return(
-				<View />
+				<ActivityIndicator
+              animating={true}
+              size='large'
+              style={styles.loading} />
 			)
 		}
 
@@ -51,9 +82,65 @@ class Feed extends Component {
 			<ListView 
 				automaticallyAdjustContentInsets={true}
 				dataSource={this.state.dataSource}
-				renderRow={this.renderFeed} />
+				renderRow={this.renderFeed}
+        renderSeparator={this.renderSeparator} 
+        style={styles.listView} />
 		)
 	}
+
+  renderSeparator(sectionID, rowID) {
+    return (
+            <View style={styles.separator} key={sectionID+rowID}/>
+        );
+  }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    padding: 12
+  },
+  separator: {
+    height: 1,
+    backgroundColor: '#CCC'
+  },
+  avatarPic: {
+    marginLeft: 4,
+    width: 36,
+    height: 36,
+    borderRadius: 16
+  },
+  listView: {
+    marginTop: 12,
+    marginBottom: 45
+  },
+  userInfo: {
+    paddingLeft: 24,
+    paddingRight: 16,
+    flex: 1,
+    flexDirection: 'column'
+  },
+  userName: {
+    fontWeight: '600',
+  },
+  repoName: {
+    fontWeight: '600',
+    paddingTop: 2
+  },
+  timeAgo: {
+    color: '#727272',
+    fontSize: 12,
+    paddingTop: 4
+  },
+  event: {
+    fontWeight: '400'
+  }, 
+  loading: {
+    flex: 1,
+    alignSelf: 'center'
+  }
+})
 
 module.exports = Feed
